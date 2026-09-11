147bebd80671cf47b58ee5cb889edf0147f3398f
Rayhan <sigmaperson2828@gmail.com>
2026-09-11 20:53:08 +0700

    fix(about): optical and mathematical centering of play icon in Group 427320837 to match pen.dev 1:1

--- src/components/about/AboutVideoCard.tsx
+++ src/components/about/AboutVideoCard.tsx
@@ -51,20 +51,23 @@ export const AboutVideoCard: React.FC<AboutVideoCardProps> = ({
         aria-label="Play video: How we work"
         className="absolute -bottom-6 -right-4 sm:-bottom-8 sm:-right-6 lg:-bottom-8 lg:-right-8 w-[120px] h-[120px] sm:w-[140px] sm:h-[140px] xl:w-[163px] xl:h-[163px] bg-brand-lime rounded-full border-[8px] sm:border-[12px] border-[#fafafa] flex items-center justify-center shadow-[0px_45px_66px_-10px_rgba(0,0,0,0.44)] cursor-pointer z-20 shrink-0"
       >
-        {/* solar:play-bold vector icon */}
+        {/* solar:play-bold vector icon with exact 1:1 pen.dev positioning */}
         <svg
           data-name="solar:play-bold"
           width="44"
           height="44"
-          viewBox="0 0 24 24"
+          viewBox="0 0 44 44"
           fill="none"
           xmlns="http://www.w3.org/2000/svg"
-          className="w-7 h-7 sm:w-9 sm:h-9 xl:w-11 xl:h-11 translate-x-0.5"
+          className="w-8 h-8 sm:w-10 sm:h-10 xl:w-[44px] xl:h-[44px]"
         >
-          <path
-            d="M17.409 7.35331c0.4803 0.25541 0.88205 0.63669 1.16221 1.10298 0.28016 0.46629 0.42817 1.00003 0.42817 1.54402 0 0.54399-0.14801 1.07772-0.42817 1.54401-0.28016 0.46629-0.68191 0.84757-1.16221 1.10299l-12.812 6.967c-2.063 1.122-4.597-0.338-4.597-2.646l0-13.935c0-2.31 2.534-3.769 4.597-2.648l12.812 6.968z"
-            fill="#000000"
-          />
+          <g transform="translate(7.333, 3.666)">
+            <path
+              d="M17.409 7.35331c0.4803 0.25541 0.88205 0.63669 1.16221 1.10298 0.28016 0.46629 0.42817 1.00003 0.42817 1.54402 0 0.54399-0.14801 1.07772-0.42817 1.54401-0.28016 0.46629-0.68191 0.84757-1.16221 1.10299l-12.812 6.967c-2.063 1.122-4.597-0.338-4.597-2.646l0-13.935c0-2.31 2.534-3.769 4.597-2.648l12.812 6.968z"
+              transform="scale(1.83333)"
+              fill="#000000"
+            />
+          </g>
         </svg>
       </button>
     </div>
